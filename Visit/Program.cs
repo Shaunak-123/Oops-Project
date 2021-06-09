2499a2e76843a48ddc8ed7a8ae91388d4e22e444
Shaunak-123 <sk77monty@gmail.com>
2021-06-09 13:07:39 +0530

    first Commit

---
+++ Visit/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Visit
+{
+    abstract class GuestVisit
+    {
+        public void Guestwelcomemessage()
+        {
+            Console.WriteLine("Welcome to our house");
+        }
+
+        public void GuestName()
+        {
+            Console.WriteLine("Guest name is Ramu");
+        }
+        public abstract void purposeofVisit();
+    }
+
+    class Program:GuestVisit
+    {
+        static void Main(string[] args)
+        {
+            Program h1 = new Program();
+            h1.Guestwelcomemessage();
+            Console.ReadKey();
+        }
+
+        public override void purposeofVisit()
+        {
+            Console.WriteLine("Just came here to spend some time");
+            
+        }
+
+    }
+}
